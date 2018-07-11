e5af749ee4803b6b03d95432165f91fb95ddcb08
DESKTOP-8C4OQDM\E <shinsr94@gmail.com>
2018-07-11 16:32:45 +0900

    [add and modity] 자바코드 수정본

--- app/src/main/java/com/example/ma/seat/register_restaurant.java
+++ app/src/main/java/com/example/ma/seat/register_restaurant.java
@@ -2,6 +2,8 @@ package com.example.ma.seat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class register_restaurant extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class register_restaurant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_restaurant);
+    }
+
+
+    public void onClick(View view){
+        Intent intent = new Intent(this, view_map.class);
+        startActivity(intent);
     }
 }
