2d5290b8d1898881d70177851055ebc45072750f
koco0371 <40329802+koco0371@users.noreply.github.com>
2018-08-07 22:28:46 +0900

    Add files via upload

---
+++ main/java/com/example/apply_picture/apply_picture.java
@@ -0,0 +1,13 @@
+package com.example.apply_picture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class apply_picture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_picture);
+    }
+}
